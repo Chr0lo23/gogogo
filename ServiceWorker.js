df0789d3dde2f6ae1052a469b7df00ab74649bb4
Chr0lo <163361631+Chr0lo@users.noreply.github.com>
2024-08-04 15:57:05 +0300

    gf

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,29 +5,38 @@ const contentToCache = [
     "Build/TekTest.data",
     "Build/TekTest.wasm",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
+
     e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
     })());
 });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+        if (e.request.method !== 'GET') {
+            // Nu cache-ui cererile POST și altele
+            console.log(`[Service Worker] Fetching resource: ${e.request.url} (Not caching POST)`);
+            return fetch(e.request);
+        }
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+        // Încercă să găsești resursa în cache
+        let response = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (response) {
+            return response;
+        }
+
+        // Fetch resursa și cache-ui-o
+        response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
     })());
 });
