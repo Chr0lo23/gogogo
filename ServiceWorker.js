4a04bf05355289f0133540bb0e81fa2de74549d1
Chr0lo <163361631+Chr0lo@users.noreply.github.com>
2024-08-05 21:17:11 +0300

    kjh

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-TekTest-1.0";
+const cacheName = "Tek-WebglTon-0.3";
 const contentToCache = [
-    "Build/TekTest.loader.js",
-    "Build/TekTest.framework.js",
-    "Build/TekTest.data",
-    "Build/TekTest.wasm",
+    "Build/test.loader.js",
+    "Build/test.framework.js",
+    "Build/test.data",
+    "Build/test.wasm",
     "TemplateData/style.css"
 
 ];
